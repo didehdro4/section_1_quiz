d7ca52f4d0d7813c170d4cb53c1c85d352553ee9
didehdro4 <yangj079@newschool.edu>
2015-10-15 21:26:20 -0400

    Test

--- sketch.js
+++ sketch.js
@@ -1,16 +1,51 @@
 function setup() {
 	// create a place to draw
 	createCanvas(640, 360);
+	noStroke();
 }
 
 function draw() {
-	// clear the background
-	background(50, 50, 50);
+	// draw background
+	background(0, 0, 70);
+	if(mouseIsPressed){
+	    background(random(0, 200), random(0, 200), random(0, 200));
+	}
 
-	// set the drawing style
+	// draw ground, trees
+	fill(10, 50, 25);
+	for (var x = 0; x < 10; x++) {
+		triangle((x * 64) - 50, 360, (x * 64) + 32, 200, (x * 64) + 114, 360);
+	}
+
+
+	fill(10, 90, 50);
+	rect(0, 270, 640, 360);
+
+	// draw ufo
 	fill(100, 100, 100);
-	stroke(255, 255, 255);
+	ellipse(mouseX, mouseY - 60, 100, 50);
+	ellipse(mouseX, mouseY - 80, 50, 60);
 
-	// draw an ellipse
-	ellipse(320, 180, 100, 100);
+	//draw ufo windows
+	fill(75, 75, 75);
+	ellipse(mouseX - 50, mouseY - 60, 10, 10);
+	ellipse(mouseX - 25, mouseY - 60, 10, 10);
+	ellipse(mouseX, mouseY - 60, 10, 10);
+	ellipse(mouseX + 25, mouseY - 60, 10, 10);
+	ellipse(mouseX + 50, mouseY - 60, 10, 10);
+
+	//make ufo flash
+	if (mouseIsPressed) {
+
+		fill(random(0, 255), random(0, 255), random(0, 255));
+		ellipse(mouseX - 50, mouseY - 60, 10, 10);
+		ellipse(mouseX - 25, mouseY - 60, 10, 10);
+		ellipse(mouseX, mouseY - 60, 10, 10);
+		ellipse(mouseX + 25, mouseY - 60, 10, 10);
+		ellipse(mouseX + 50, mouseY - 60, 10, 10);
+		ellipse(mouseX, mouseY - 40, 30, 5);
+
+	//make spotlight
+	  ellipse(mouseX, 310, 65, 20);
+	}
 }
